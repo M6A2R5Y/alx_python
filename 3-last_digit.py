2f71639a76d751065bcc13ced6bc2061746b0931
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-07-20 03:20:45 +0300

    this is a tough skin py

---
+++ 3-last_digit.py
@@ -0,0 +1,7 @@
+#!/bin/bash/python3
+import random
+number =random.randint(10000,-10000)
+if .format(number[-1:]) > 5:
+    print("Last digit of", number,"is 5 and is  greater than 5")
+elif .format(number[-1:]) == 0:
+
