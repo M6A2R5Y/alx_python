880d32a9f12d70b7a97f70e8d4075856ef2ea3ec
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-07-25 07:00:09 +0300

    awesome

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for digits in range(10):
     for digits in range(digits + 1, 10):
         print("{:02d}".format(digits * 10 + digits), end=", " if digits < 8 or digits < 9 else "\n",)
