d5a5da334357b33c050e303d2c95efdf9761a109
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-07-25 06:20:17 +0300

    this is new

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for number in range(100):
-    if number <= 99:
-        print("{:02d}".format(number),end=", ")
-    else: "\n"
+for num in range(100):
+    print("{:02d}".format(num), end=", " if num < 99 else "\n",)
+
