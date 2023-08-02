5d41e5adc12936ca0d04a92110b79c4cd90bb23c
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-08-02 17:56:22 +0300

    fibonacci

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -6,9 +6,9 @@ def fibonacci_sequence(n):
     if n == 1:
         return [0]
 
-    fibonacci_list = [0, 1]
-    while len(fibonacci_list) < n:
-        next_num = fibonacci_list[-1] + fibonacci_list[-2]
-        fibonacci_list.append(next_num)
+    sequence = [0, 1]
+    while len(sequence) < n:
+        next_num = sequence[-1] + sequence[-2]
+        sequence.append(next_num)
 
-    return fibonacci_list
+    return sequence
