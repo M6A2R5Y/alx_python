2084064f84b21b55f4e2d23ce883fd445c13bcd4
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-08-12 18:28:40 +0300

    the matrix is finally resolved

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for cool in range(len(line)):
             if cool == len(line) - 1:
-                print("{:d}".format(line[col]), end="")
+                print("{:d}".format(line[cool]), end="")
             else:
-                print("{:d}".format(line[col]), end="")
+                print("{:d}".format(line[cool]), end="")
         print()
