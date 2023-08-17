23049ab022a52d0f7d97a8e9d0b269ad14d029c0
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-08-18 02:30:05 +0300

    here we go

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
-Url = "https://alu-intranet.hbtn.io/status"
-response = requests.get(Url)
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
 # Get the response from the URL
 print('Body response:')
 print(f'\t- type: {type(response.text)}')
