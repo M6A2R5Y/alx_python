c8bf6da66415f8b75cff1f823ff7fddebeaaeaae
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-08-18 02:25:29 +0300

    we move regardless

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,3 +2,7 @@
 import requests
 Url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(Url)
+# Get the response from the URL
+print('Body response:')
+print(f'\t- type: {type(response.text)}')
+print(f'\t- content: {response.text}')
