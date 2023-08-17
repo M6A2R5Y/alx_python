c10a76a3954da5f36f0e9b56120aa459148077e5
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-08-18 01:12:32 +0300

    new changes in 6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
 """
-This module implements a Rectangle object
-"""
+This module implements a Rectangle object that inherits from BaseGeometry class.
 
+Classes:
+    Rectangle: Represents a rectangle with width and height.
+
+"""
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle implementation
     """
+    A class representing a rectangle.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+
+    Methods:
+        __init__(width, height):
+            Initializes a new Rectangle instance with the specified width and height.
+
+    """
+
     def __init__(self, width, height):
-        """initialisation
+        """
+        Initialize a Rectangle instance with the specified width and height.
 
         Args:
-            width (int): width
-            height (int): height
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            ValueError: If either width or height is not a positive integer.
+
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
