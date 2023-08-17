cee6b16085dc001dc155eddf9cd0f061162a6e4f
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-08-18 01:47:44 +0300

    se sytem

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,21 @@
-#!/usr/bin/python3
-"""
-This module implements an object skeleton for BaseGeometry
-"""
-
-
 class BaseGeometry:
-    """skeleton
     """
-    def display(self):
-        return
+    BaseGeometry Class
+    -----------------
+
+    An empty base class that serves as a foundation for defining geometric classes.
+
+    This class can be used as a starting point for creating more specialized geometry classes.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    """
+    pass
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+    print(dir(bg))
